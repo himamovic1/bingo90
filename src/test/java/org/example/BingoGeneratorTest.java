d1093a916c28624e6e7bfc3f3e46ee1b4e58eb3e
Haris Imamović <imamovic.haris@outlook.com>
2023-08-10 18:51:29 +0200

    Tests to confirm all numbers appear in the ticket

--- src/test/java/org/example/BingoGeneratorTest.java
+++ src/test/java/org/example/BingoGeneratorTest.java
@@ -6,10 +6,7 @@ import org.example.rows.ShuffleRangeGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,7 +45,7 @@ public class BingoGeneratorTest {
     }
 
     @Test
-    @Timeout(value = 10)
+    @Timeout(value = 1)
     public void generateBingo90__shuffle_10kTickets_valid() {
         // given
         var sut = new BingoGenerator(SHUFFLE_RANGE_GENERATOR);
@@ -89,7 +86,7 @@ public class BingoGeneratorTest {
     }
 
     @Test
-    @Timeout(value = 10)
+    @Timeout(value = 1)
     public void generateBingo90__combinator_10kTickets_valid() {
         // given
         var sut = new BingoGenerator(COMBINATOR_RANGE_GENERATOR);
@@ -106,20 +103,32 @@ public class BingoGeneratorTest {
     private void assertIsValidBingo90Board(int[][] board) {
         assertThat(board).hasDimensions(18, 9);
 
-        // check it has exactly four blanks per row
-        // and correct number of blanks per column
         var blanksPerRow = new int[18];
         var blanksPerCol = new int[9];
+        var numbers = new HashSet<Integer>();
+
         for (int row = 0; row < 18; row++)
             for (int col = 0; col < 9; col++)
                 if (board[row][col] == -1) {
+                    // check it has exactly four blanks per row
+                    // and correct number of blanks per column
                     blanksPerRow[row]++;
                     blanksPerCol[col]++;
+                } else {
+                    // to confirm it contains all numbers between 1 and 90
+                    numbers.add(board[row][col]);
                 }
 
+        // assert it contains all numbers which is guaranteed for 6 strip bingo 90
+        assertThat(numbers).hasSize(90);
+        assertThat(Collections.min(numbers)).isEqualTo(1);
+        assertThat(Collections.max(numbers)).isEqualTo(90);
+
+        // assert each row has exactly 4 blank spaces
         assertThat(Arrays.stream(blanksPerRow).boxed().toList())
                 .isEqualTo(Collections.nCopies(18, 4));
 
+        // assert each column has exactly needed number of blanks
         assertThat(Arrays.stream(blanksPerCol).boxed().toList())
                 .isEqualTo(List.of(9, 8, 8, 8, 8, 8, 8, 8, 7));
     }
